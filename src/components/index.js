30090b4e7f82601417edcce50d0c50d621f9bb71
vryasho <velro.yasho@gmail.com>
2023-10-15 17:17:20 -0500

    fix: Features renamed Feature

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 export { default as Article } from "./article/Article";
 export { default as Brand } from "./brad/Brand";
 export { default as CTA } from "./cta/CTA";
-export { default as Features } from "./features/Features";
+export { default as Feature } from "./feature/Feature";
 export { default as Navbar } from "./navbar/Navbar";
