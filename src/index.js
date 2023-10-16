9292cc39c2f91082cb1c41811801112a8d9c4a02
vryasho <velro.yasho@gmail.com>
2023-10-15 22:39:23 -0500

    refactor: index refactored for REACT 18

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
+// VERSION REACT 17
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import App from "./App";
+// ReactDOM.render(<App />, document.getElementById("root"));
+
+// VERSION REACT 18 +
+
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./App";
-ReactDOM.render(<App />, document.getElementById("root"));
+import "./index.css";
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App></App>);
